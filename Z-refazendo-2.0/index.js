10a7f3aa3b2bc2c8ede06f88311345a25b287147
Gabriel <gabrieldevspace@gmail.com>
2024-01-28 01:19:43 -0300

    refazendo 2.0

---
+++ Z-refazendo-2.0/index.js
@@ -0,0 +1,14 @@
+//var cor = window.prompt("Qual a cor da sua página");
+
+if (cor == "preto"){
+   document.body.style.backgroundColor ="black"
+}
+if (cor == "vermelho"){
+    document.body.style.backgroundColor ="red"
+ }
+ if (cor == "amarelo"){
+    document.body.style.backgroundColor ="yellow"
+ }
+ if (cor == "azul"){
+    document.body.style.backgroundColor ="blue"
+ }
